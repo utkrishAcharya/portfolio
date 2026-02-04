a0498e94f2df55590ca99b14bb32d2e0ef5b125f
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-04 11:08:04 +0545

    Refactor theme toggle logic for clarity

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -32,14 +32,18 @@ themeToggleBtn.addEventListener('click', function() {
             
         } else {
             document.documentElement.classList.remove('dark');
+            
             localStorage.setItem('color-theme', 'light');
         }
     } else {
         if (document.documentElement.classList.contains('dark')) {
+            
             document.documentElement.classList.remove('dark');
+            
             localStorage.setItem('color-theme', 'light');
         } else {
             document.documentElement.classList.add('dark');
+            
             localStorage.setItem('color-theme', 'dark');
         }
     }
