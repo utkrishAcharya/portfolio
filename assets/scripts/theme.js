25c987bad49faaab70aacb7eda0f6fe0877347b6
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-24 20:05:28 +0545

    Update theme.js

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,7 +1,6 @@
 // Theme toggle logic for portfolio
 const themeToggleBtn = document.getElementById('theme-toggle');
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
