8a1d17ba15d9b8c14b3a60284a77440d4b293875
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-24 20:05:41 +0545

    Update theme.js

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -2,7 +2,6 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
     
