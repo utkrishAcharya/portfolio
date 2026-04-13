002e4270e848c405702f21b5f7fddc30dd540999
utkrishAcharya <acharyautkrish14@gmail.com>
2026-04-13 20:26:34 +0545

    Update theme.js

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -9,6 +9,7 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
     
 } else {
     
+    
     document.documentElement.classList.remove('dark');
     
     themeToggleDarkIcon.classList.remove('hidden');
