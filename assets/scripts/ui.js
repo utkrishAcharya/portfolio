95f4d09959ae9a16012201c4503c21959793f68a
utkrishAcharya <acharyautkrish14@gmail.com>
2026-05-29 20:14:45 +0545

    Update ui.js

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -26,6 +26,7 @@ if (mobileMenuButton && mobileMenu) {
 // Dynamic year in footer
 const yearSpan = document.getElementById('current-year');
 if (yearSpan) {
+  
   yearSpan.textContent = new Date().getFullYear();
 }
 
